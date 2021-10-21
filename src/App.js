9ff30797ca0c8cc3393dc84a6fb67e9a623e0fc5
Sagar-57 <sagar05.ms@gmail.com>
2021-10-21 22:49:18 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./style.css";
 import 'bootstrap';
+import Navbar from './components/includes/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
  function App() {
   return (
     <div>
+      <Navbar/>
    <Home/>
    <About/>
    <Contact/> 
